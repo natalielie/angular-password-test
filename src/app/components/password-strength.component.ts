424c7d92aa391cc93ec7fc8d29986390e6c35f52
Наталія Малищенко <nataliia.malyshchenko@nure.ua>
2023-07-20 13:11:45 +0300

    password-strength.component algorithms fixed

--- src/app/components/password-strength.component.ts
+++ src/app/components/password-strength.component.ts
@@ -7,71 +7,68 @@ import { Component, EventEmitter, Input, OnChanges, Output, SimpleChange } from 
 })
 
 export class PasswordStrengthComponent implements OnChanges {
-  bar0!: string; 
-  bar1!: string;
-  bar2!: string;
+  section0!: string; 
+  section1!: string;
+  section2!: string;
 
+  // bounds to the password control on the form
   @Input()
     public passwordToCheck: string | undefined | null;
 
+  // event to emit boolean value
   @Output() passwordStrength = new EventEmitter<boolean>();
 
-  private colors = ['#730f0f', '#d69d0d', '#32a852'];
+  colors = ['#730f0f', '#d69d0d', '#32a852'];
 
   message!: string;
   messageColor!: string;
 
   checkStrength(password: string) {
-    // 1
-    let force = 0;
 
-    // 2
+    let strength = 0;
+
+    // regex to check the password strength
     const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
-    const lowerLetters = /[a-z]+/.test(password);
-    const upperLetters = /[A-Z]+/.test(password);
+    const letters = /[A-Za-z]+/.test(password);
     const numbers = /[0-9]+/.test(password);
     const symbols = regex.test(password);
 
-    // 3
-    const flags = [lowerLetters, upperLetters, numbers, symbols];
+    const flags = [letters, numbers, symbols];
 
-    // 4
-    let passedMatches = 0;
+    let successMatchCount = 0;
     for (const flag of flags) {
-      passedMatches += flag === true ? 1 : 0;
+      successMatchCount += flag === true ? 1 : 0;
     }
 
-    // 5
-    force += 2 * password.length + (password.length >= 10 ? 1 : 0);
-    force += passedMatches * 10;
+    // setting the strength level
+    strength += 2 * password.length + (password.length >= 10 ? 1 : 0);
+    strength += successMatchCount * 10;
 
-    // 6
-    force = password.length <= 6 ? Math.min(force, 10) : force;
+    //a special case for painting all sections red
+    strength = password.length <= 7 ? Math.min(strength, 5) : strength;
 
-    // 7
-    force = passedMatches === 1 ? Math.min(force, 10) : force;
-    force = passedMatches === 2 ? Math.min(force, 20) : force;
-    force = passedMatches === 3 ? Math.min(force, 30) : force;
-   
-    if(password.length > 0 && password.length < 8)
-    force = 5;
+    //depending on passed regex, set the strehgth
+    strength = successMatchCount === 1 ? Math.min(strength, 10) : strength;
+    strength = successMatchCount === 2 ? Math.min(strength, 20) : strength;
+    strength = successMatchCount === 3 ? Math.min(strength, 30) : strength;
 
-    return force;
+    return strength;
   }
 
   ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
     const password = changes['passwordToCheck'].currentValue;
 
-    this.setBarColors(3, '#707070');
+    this.setSectionColors(3, '#707070');
 
     if (password) {
-      const pwdStrength = this.checkStrength(password);
-      pwdStrength === 30 ? this.passwordStrength.emit(true) : this.passwordStrength.emit(false);
+      const passwordStrength = this.checkStrength(password);
+      passwordStrength === 30 ? this.passwordStrength.emit(true) 
+      : this.passwordStrength.emit(false);
 
-      const color = this.getColor(pwdStrength);
-      this.setBarColors(color.index, color.color);
+      const color = this.getColor(passwordStrength);
+      this.setSectionColors(color.index, color.color);
 
-      switch (pwdStrength) {
+      switch (passwordStrength) {
         case 5:
           this.message = '';
           break;
@@ -79,7 +76,7 @@ export class PasswordStrengthComponent implements OnChanges {
           this.message = 'Poor';
           break;
         case 20:
-          this.message = 'Average';
+          this.message = 'Medium';
           break;
         case 30:
           this.message = 'Good';
@@ -90,6 +87,9 @@ export class PasswordStrengthComponent implements OnChanges {
     }
   }
 
+
+  // colors management 
+  
   private getColor(strength: number) {
     let index = 0;
     let color = '';
@@ -119,9 +119,9 @@ export class PasswordStrengthComponent implements OnChanges {
     };
   }
 
-  private setBarColors(count: number, color: string) {
+  private setSectionColors(count: number, color: string) {
     for (let n = 0; n < count; n++) {
-      (this as any)['bar' + n] = color;
+      (this as any)['section' + n] = color;
     }
   }
 }
